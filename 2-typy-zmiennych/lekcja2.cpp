3112088e6b257feb1db751b28f676b20152d7247
wsparcie.exe@gmail.com <wsparcie.exe@gmail.com>
2020-09-28 21:25:57 +0200

    first

---
+++ 2-typy-zmiennych/lekcja2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main(){
+cout << "Ile masz lat? \n";
+int lata;
+cin >> lata;
+cout << "Masz lat: "<<lata<< " lat";
+
+cin.ignore(); //nie oczekuje na znak eter
+getchar(); //oczekuje na znak eter
+	
+getchar();
+return 0;
+
+/* Typy zmiennych
+
+int //liczby calkowite
+char //wszytskie znaki
+string //tylko teskt
+double //liczby rzeczywiste
+float //zmienna przecinkowa 
+ 
+
+
+
+*/
+	
+	
+}
+
