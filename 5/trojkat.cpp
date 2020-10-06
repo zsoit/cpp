f5b6f70b51876650e54e5c46984f6b48612388b9
zsoit <wsparcie.exe@gmail.com>
2020-10-06 11:22:28 +0200

    5

---
+++ 5/trojkat.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdio>
+#include <math.h>
+#include <locale.h>
+#include <windows.h>
+using namespace std;
+int main(){
+	setlocale(LC_CTYPE, "Polish");
+	SetConsoleTitleA("Pole trójk¹tu");
+	
+
+cout<<"=================================== \n";
+cout<<"Pole trójk¹tu \n";
+cout<<"=================================== \n";
+
+
+float a, b, h, pole;
+
+
+
+
+
+cin.ignore(); 
+return 0;
+}
+
+
+//Napisz program, który bêdzie oblicza³ pole i obwód prostk¹ta pamietajac ze a i b musi btc wieksze od 0;
+
+
